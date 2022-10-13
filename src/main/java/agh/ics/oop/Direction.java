df5cf48874eb3f42d5062f2a009327e19b0b1124
krisx303 <57154118+krisx303@users.noreply.github.com>
2022-10-13 14:06:23 +0200

    Simple change
    Added output variable for Direction

--- src/main/java/agh/ics/oop/Direction.java
+++ src/main/java/agh/ics/oop/Direction.java
@@ -3,20 +3,26 @@ package agh.ics.oop;
 import java.util.Arrays;
 
 public enum Direction {
-    FORWARD("f"),
-    BACKWARD("b"),
-    RIGHT("r"),
-    LEFT("l");
+    FORWARD("f", "Zwierzak idzie do przodu"),
+    BACKWARD("b", "Zwierzak idzie do tyłu"),
+    RIGHT("r", "Zwierzak skręca w prawo"),
+    LEFT("l", "Zwierzak skręca w lewo");
 
     private final String label;
+    private final String output;
 
-    Direction(String label) {
+    Direction(String label, String output) {
         this.label = label;
+        this.output = output;
     }
 
     public static Direction getDirectionByLabel(String str){
         return Arrays.stream(Direction.values()).
                 filter(direction -> direction.label.equals(str)).
                 findFirst().orElse(FORWARD);
+    }
+
+    public String getOutput() {
+        return output;
     }
 }
--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -6,26 +6,11 @@ public class World {
     public static void main(String[] args) {
         System.out.println("system wystartował");
         Direction[] directions = Arrays.stream(args).map(Direction::getDirectionByLabel).toArray(Direction[]::new);
-//        int len = args.length;
-//        Direction[] directions = new Direction[len];
-//        for (int i = 0; i < args.length; i++) {
-//            directions[i] = Direction.getDirectionByLabel(args[i]);
-//        }
         run(directions);
         System.out.println("system zakończył działanie");
     }
 
     private static void run(Direction[] directions){
-//        String joined = Arrays.stream(tab).map(Object::toString)
-//                .collect(Collectors.joining(", "));
-//        System.out.println(joined);
-        Arrays.stream(directions).forEach(command -> {
-            switch (command){
-                case FORWARD -> System.out.println("Zwierzak idzie do przodu");
-                case BACKWARD -> System.out.println("Zwierzak idzie do tyłu");
-                case RIGHT -> System.out.println("Zwierzak skręca w prawo");
-                case LEFT -> System.out.println("Zwierzak skręca w lewo");
-            }
-        });
+        Arrays.stream(directions).forEach(direction -> System.out.println(direction.getOutput()));
     }
 }
